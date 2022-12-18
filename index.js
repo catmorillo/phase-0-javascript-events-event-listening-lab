d516644f820643f7022f4ac986d21e210a5db074
catherine <catmorillo@hotmail.com>
2022-12-18 18:05:09 -0500

    commit tree c5d83745c06abd6e5113b566cb39ffbf06428c08

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+function addingEventListener(clickAlert) {
+
+const input = document.getElementById('button');
+
+function clickAlert(addingEventListener) {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
 }
